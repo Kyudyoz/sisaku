f2471411203ae0bd6ffe4f0d400ddb5c4a3a3ccd
SabRyan-Holmes <sabrian101maulana@gmail.com>
2023-12-03 11:15:00 +0700

    Class Model Rekap

---
+++ lib/models/rekap.dart
@@ -0,0 +1,35 @@
+import 'package:drift/drift.dart';
+
+@DataClassName('Rekap')
+class Rekaps extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  // Nama Rekap
+  TextColumn get name => text()();
+  // Tanggal Durasi
+  DateTimeColumn get startDate => dateTime()();
+  DateTimeColumn get endDate => dateTime()();
+
+  // Database dibuat
+  DateTimeColumn get createdAt => dateTime()();
+  DateTimeColumn get updatedAt => dateTime()();
+
+  // Jumlah berapa banyak transaksi
+  IntColumn get totalTransactions => integer().nullable()();
+
+  // Total, Rata-rata, Sisa jumlah uang transaksi
+  IntColumn get totalExpense => integer().nullable()();
+  IntColumn get totalIncome => integer().nullable()();
+  IntColumn get sisa => integer().nullable()();
+  RealColumn get averageExpense => real().nullable()();
+  RealColumn get averageIncome => real().nullable()();
+}
+
+class CustomRekap {
+  DateTime startDate;
+  DateTime endDate;
+
+  CustomRekap({
+    required this.startDate,
+    required this.endDate,
+  });
+}
